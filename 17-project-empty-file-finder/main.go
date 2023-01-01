103ba6578985675c108be7a156158653ae7f0b57
hsimao <e087754958@gmail.com>
2023-01-01 22:30:03 +0800

    feat: empty file finder

--- 17-project-empty-file-finder/main.go
+++ 17-project-empty-file-finder/main.go
@@ -19,13 +19,28 @@ func main() {
 		return
 	}
 
-	names := []string{}
+	var total int
+	for _, file := range files {
+		if file.Size() == 0 {
+			total += len(file.Name()) + 1
+		}
+	}
+	fmt.Printf("Total required space: %d bytes.\n", total)
+
+	names := make([]byte, 0, total)
 
 	for _, file := range files {
 		if file.Size() == 0 {
 			name := file.Name()
-			names = append(names, name)
-			fmt.Println("names", names)
+			names = append(names, name...)
+			names = append(names, '\n')
 		}
 	}
+
+	err = ioutil.WriteFile("out.text", names, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s", names)
 }
